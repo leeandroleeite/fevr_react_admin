09cf839b3c30275301e706cbd358cd8f4590e2aa
Leandro Leite <leeandro.leeite@gmail.com>
2022-04-11 20:28:42 +0100

    fix: Removed axios and fixed deprecated ReactDOM.render

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+
   return (
     <div className="App">
-      <p>Hello World</p>
+      <h1>Hello World</h1>
     </div>
   );
 }
